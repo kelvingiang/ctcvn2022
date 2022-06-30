f83cbe92b08f3a4182f3b54c31b768856ec5ea15
kelvin <giaminh0265@yahoo.com>
2022-06-30 14:57:05 +0700

    test5

--- wp-content/themes/suite/test.php
+++ wp-content/themes/suite/test.php
@@ -15,10 +15,14 @@
     <h2>Lorem ipsum dolor sit.</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, delectus?</p>
 
-    <h3>Lorem, ipsum.</h3>
+    <h3>Lorem, ipsum.</h3>git
     <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid doloribus deserunt reiciendis tempore doloremque in, consequuntur odit nobis atque eligendi accusamus fugiat ut, dicta consectetur illum nam, ea qui suscipit.</label>
 
     <h3>Lorem, ipsum.</h3>
+    <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid doloribus deserunt reiciendis tempore doloremque in, consequuntur odit nobis atque eligendi accusamus fugiat ut, dicta consectetur illum nam, ea qui suscipit.</label>
+
+
+    <h5>Lorem, ipsum.</h5>git
     <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid doloribus deserunt reiciendis tempore doloremque in, consequuntur odit nobis atque eligendi accusamus fugiat ut, dicta consectetur illum nam, ea qui suscipit.</label>
 </body>
 
