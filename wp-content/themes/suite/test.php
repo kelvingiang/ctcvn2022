9d194b47ba21bb83e51944c65d841b8399786f97
kelvin <giaminh0265@yahoo.com>
2022-06-30 14:38:18 +0700

    test 2

--- wp-content/themes/suite/test.php
+++ wp-content/themes/suite/test.php
@@ -11,6 +11,8 @@
 <body>
     <h1>haaaaaaaaa</h1>
     <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos dolorum culpa deleniti, voluptatem ex a dolores illum aliquid aliquam architecto alias eos error, id recusandae quia eaque, quaerat adipisci eum!</label>
+    <h2>Lorem, ipsum dolor.</h2>
+    <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente esse iure minus quod iste accusamus debitis eum nostrum saepe eos, nobis cupiditate odio! Nisi commodi eum illo enim quisquam placeat?</label>
 </body>
 
 </html>
